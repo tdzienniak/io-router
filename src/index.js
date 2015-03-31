56f0afce43211535ca26da08e1f6697b77c9adc6
Tymoteusz Dzienniak <dzienniak.tymoteusz@gmail.com>
2015-03-31 12:41:47 +0200

    Indentation converted to spaces

--- src/index.js
+++ src/index.js
@@ -3,95 +3,95 @@ var debug = require('debug')('io-router');
 var Request = require('./request');
 
 module.exports = function (io, options) {
-	'use strict';
+    'use strict';
 
-	var routes = {};
+    var routes = {};
 
-	if (is.not.object(options)) {
-		options = {};
-	}
+    if (is.not.object(options)) {
+        options = {};
+    }
 
-	function route (name, thing, options) {
-		if (is.not.unemptyString(name)) {
-			debug('route name should be an unempty string');
-			return;
-		}
+    function route (name, thing, options) {
+        if (is.not.unemptyString(name)) {
+            debug('route name should be an unempty string');
+            return;
+        }
 
-		if (is.not.object(options)) {
-			options = {};
-		}
+        if (is.not.object(options)) {
+            options = {};
+        }
 
-		if (options.trigger) {
-			if (!(name in routes)) {
-				debug('can\'t forward - route %s doesn\'t exist', name);
-				return;
-			}
+        if (options.trigger) {
+            if (!(name in routes)) {
+                debug('can\'t forward - route %s doesn\'t exist', name);
+                return;
+            }
 
-			routes[name](thing);
+            routes[name](thing);
 
-			return;
-		}
+            return;
+        }
 
-		if (is.object(thing)) {
-			for (var routeName in thing) {
-				route(name + ':' + routeName, thing[routeName])
-			}
+        if (is.object(thing)) {
+            for (var routeName in thing) {
+                route(name + ':' + routeName, thing[routeName])
+            }
 
-			return;
-		}
+            return;
+        }
 
-		if (is.not.function(thing)) {
-			debug('you must provide callback to register route');
+        if (is.not.function(thing)) {
+            debug('you must provide callback to register route');
 
-			return;
-		}
+            return;
+        }
 
-		routes[name] = thing;
+        routes[name] = thing;
 
-		return;
-	}
+        return;
+    }
 
-	function init(socket) {
-		for (var name in routes) {
-			
-			var callback = routes[name];
+    function init(socket) {
+        for (var name in routes) {
+            
+            var callback = routes[name];
 
-			socket.on(name, function (data, respond) {
-				if (is.function(data)) {
-					respond = data;
-					data = undefined;
-				}
+            socket.on(name, function (data, respond) {
+                if (is.function(data)) {
+                    respond = data;
+                    data = undefined;
+                }
 
-				var request = {
-					data: data,
-					socket: socket,
-					session: socket.handshake.session,
-					sessionID: socket.handshake.sessionID,
-					headers: socket.handshake.headers,
-					cookies: socket.handshake.cookies,
-					respond: (is.function(respond)) ? respond : function () {}
-				};
-				
-				request.io = new Request(io, request);
+                var request = {
+                    data: data,
+                    socket: socket,
+                    session: socket.handshake.session,
+                    sessionID: socket.handshake.sessionID,
+                    headers: socket.handshake.headers,
+                    cookies: socket.handshake.cookies,
+                    respond: (is.function(respond)) ? respond : function () {}
+                };
+                
+                request.io = new Request(io, request);
 
-				return callback(request);
-			});
-		}
-	}
+                return callback(request);
+            });
+        }
+    }
 
-	if (options.initOnConnection) {
-		io.on('connection', function (socket) {
-			init(socket);
-		});
-	}
+    if (options.initOnConnection) {
+        io.on('connection', function (socket) {
+            init(socket);
+        });
+    }
 
-	//setting up router on socket.io instance
-	io.router = {
-		init: init,
-		route: route
-	};
+    //setting up router on socket.io instance
+    io.router = {
+        init: init,
+        route: route
+    };
 
-	io.route = route;
+    io.route = route;
 
-	return io.router;
+    return io.router;
 }
--- src/request.js
+++ src/request.js
@@ -1,20 +1,20 @@
 var extend = require('node.extend');
 
 function Request(io, request) {
-	this.io = io;
-	this.request = request;
-	this.socket = request.socket;
+    this.io = io;
+    this.request = request;
+    this.socket = request.socket;
 }
 
 extend(Request.prototype, {
-	broadcast: function (event, message) {
-		this.socket.broadcast.emit(event, message);
-	},
-	route: function (name) {
-		this.io.route(name, this.request, {
-			trigger: true
-		});
-	}
+    broadcast: function (event, message) {
+        this.socket.broadcast.emit(event, message);
+    },
+    route: function (name) {
+        this.io.route(name, this.request, {
+            trigger: true
+        });
+    }
 });
 
 module.exports = Request;
